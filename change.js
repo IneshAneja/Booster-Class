1ff81d6250f36ed28f04f1251cd9c36640b74067
IneshAneja <86083858+IneshAneja@users.noreply.github.com>
2021-09-27 18:48:48 -0700

    Add files via upload

---
+++ change.js
@@ -0,0 +1,9 @@
+function gaming(){
+   window.location = "games.html"; 
+}
+function rubix(){
+    window.location = "rubix.html";
+}
+function logout(){
+    window.location = "page_choose.html";
+}
